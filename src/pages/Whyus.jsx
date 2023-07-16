5771ef19fbddca5cb0a03ca3fbe7b13c481451e0
Ruchika30 <sood.ruchika@siesgst.ac.in>
2023-07-16 15:23:22 +0530

    added text

--- src/pages/Whyus.jsx
+++ src/pages/Whyus.jsx
@@ -37,23 +37,43 @@ const WhyUs = () => {
                 {/* banner */}
                 <BannerComponent images={bannerItems} />
 
-                <div className='flex flex-col items-center mt-6' id="#tournaments">
-                    <Heading title="TOURNAMENTS" />
+                <div className='flex-col md:flex mt-8 px-40 py-14' id="#tournaments">
 
-                    <p>Register for the tournament you are willing to participate</p>
-                    <div className='flex'>
+                    <div className='py-14 flex flex-col'>   
+                        <div className='flex justify-center'> 
+                            <Heading title="Why Us" />
+                        </div>
+                        <div className='text-center'>
                         <Typography>
-                            <a className='px-5 py-3 mr-10 bg-green-500 rounded-md my-8' target="_blank" href='https://forms.gle/xKyMm44dcAz5QJrw9' type="button" rel="noreferrer">
-                                <span className='font-bold text-xl'>InHouse</span>
-                            </a>
+                        SCOREGOALS believes that many players just need to be given the platform to show their abilities and talents at the higher level. Our academy offers coaching and training designed for our young aspiring, passionate soccer players between the ages of 3 and 16 looking to take their game to the next level. We at SCOREGOALS help in fulfilling a child’s dreams to come true by offering a structured all year-round programme that aims at developing skills and maximising each individual’s development.
                         </Typography>
-
-                        <Typography>
-                            <a className='px-5 py-3 bg-green-500 rounded-md my-8' target="_blank" href='https://forms.gle/Hb2naxkB8yUckfQR8' type="button" rel="noreferrer">
-                                <span className='font-bold text-xl'>Other Private Tournaments</span>
-                            </a>
-                        </Typography>
+                            <Typography>About coaches
+                        At SCOREGOALS coaches are professional football players playing at Mumbai District Association and also qualified licence holders.</Typography>
+                        </div>
                     </div>
+
+
+                    <div className='py-14 flex flex-col'>     
+                        <div className='flex justify-center flex-col'> 
+                        <div className='self-center'>
+                            <Heading title="TOURNAMENTS" />
+                        </div>
+                            <p className='self-center'>Register for the tournament you are willing to participate</p>
+                       </div> 
+                            <div className='flex-col md:flex  justify-center items-center mt-6'>
+                                <Typography className='flex justify-center'>
+                                    <a className='px-5 py-3 mb-5 bg-green-500 rounded-md' target="_blank" href='https://forms.gle/xKyMm44dcAz5QJrw9' type="button" rel="noreferrer">
+                                        <span className='font-bold text-xl text-white'>InHouse Tournaments</span>
+                                    </a>
+                                </Typography>
+
+                                <Typography className='flex justify-center'>
+                                    <a className='px-5 py-3 bg-green-500 rounded-md' target="_blank" href='https://forms.gle/Hb2naxkB8yUckfQR8' type="button" rel="noreferrer">
+                                        <span className='font-bold text-xl text-white'>Other Private Tournaments</span>
+                                    </a>
+                                </Typography>
+                            </div>
+                        </div>
 
                 </div>
 
